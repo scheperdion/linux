8249288457c621ec85dbc996bf8b5f6da538141d
neo <dion.scheper@ru.nl>
2025-03-08 20:25:45 +0100

    Fuzzing arbitrary function ieee802_11_parse_elems_full

--- tools/lkl/fuzzers/arbitraryfunction/arbitraryfunction-fuzzer.c
+++ tools/lkl/fuzzers/arbitraryfunction/arbitraryfunction-fuzzer.c
@@ -25,17 +25,44 @@
 
 #include "arbitraryfunction-fuzzer.h"
 
-void (*target)(const void *data, size_t datalen);
+struct ieee80211_elems_parse_params {
+	const uint8_t *start;
+	size_t len;
+	bool action;
+	long filter;
+	int crc;
+	void *bss;
+	int link_id;
+	bool from_ap;
+};
+/**
+* ieee802_11_parse_elems_full(const u8 *start, size_t len, bool action,
+			   struct cfg80211_bss *bss)
+*/
+void* (*target)(struct ieee80211_elems_parse_params *params);
+void (*kfree)(void* ptr);
 
 void set_addresses() {
 	void (*src_ptr) = &lkl_init;
-	int source_offset = 0x00000000494a50; // nm tools/lkl/fuzzers/arbitraryfunction/arbitraryfunction-fuzzer | grep lkl_init
-	int target_offset = 0x00000000beee70; // nm tools/lkl/fuzzers/arbitraryfunction/arbitraryfunction-fuzzer | grep x509_cert_parse
+	int source_offset = 0x0000000050ab50; // nm tools/lkl/fuzzers/arbitraryfunction/arbitraryfunction-fuzzer | grep lkl_init
+	int target_offset = 0x00000000d76810; // nm tools/lkl/fuzzers/arbitraryfunction/arbitraryfunction-fuzzer | grep ieee802_11_parse_elems_full
+	int kfree__offset = 0x000000005f9680; // nm tools/lkl/fuzzers/arbitraryfunction/arbitraryfunction-fuzzer | grep kfree
 	target = src_ptr - source_offset + target_offset;
+	kfree = src_ptr - source_offset + kfree__offset;
 }
 
 static void fuzz_arbitrary_function(const uint8_t *data, size_t size) {
-	target(data, size);
+	struct ieee80211_elems_parse_params params = {
+		.start = data,
+		.len = size,
+		.action = (data[0] & 0x01) ^ (data[0] ^ 0x10),
+		.filter = 0,
+		.crc = 0,
+		.bss = NULL,
+		.link_id = -1,
+	};
+	void* ptr = target(&params);
+	kfree(ptr);
 }
 
 static int initialize_lkl(void)
@@ -80,13 +107,13 @@ int LLVMFuzzerInitialize(int *argc, char ***argv)
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
 	static int iter;
-	uint8_t data[65535] = {0};
+	uint8_t data[4096] = {0};
 
-	if (Size > 65535)
-		Size = 65535;
+	if (Size > 4096)
+		Size = 4096;
 
 	memcpy(data, Data, Size);
-	fuzz_arbitrary_function(data, Size);
+	fuzz_arbitrary_function(Data, Size);
 	iter++;
 	if (iter > 1000) {
 		flush_coverage();
