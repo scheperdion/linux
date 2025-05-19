03e19902561ee25f5ffb0b639b3db38d36f0c6d4
neo <dion.scheper@ru.nl>
2025-05-19 20:05:42 +0200

    Working with raw gadget keyboard example

--- tools/lkl/fuzzers/outoftree/outoftree-fuzzer.c
+++ tools/lkl/fuzzers/outoftree/outoftree-fuzzer.c
@@ -97,16 +97,16 @@ static void load_usb_gadget() {
     wrstr(path, "120");          /* mA */
 
     /* 5.  Functions */
-	snprintf(path, sizeof(path), "%s/%s/functions/ecm.usb0", cfgfs, gname);
+	snprintf(path, sizeof(path), "%s/%s/functions/loopback.0", cfgfs, gname);
 	if (lkl_sys_mkdir(path, 0755)) { printf("mkdir gadget functions error"); return; }
-	snprintf(path, sizeof(path), "%s/%s/functions/ecm.usb0/host_addr", cfgfs, gname);
+	snprintf(path, sizeof(path), "%s/%s/functions/loopback.0/host_addr", cfgfs, gname);
     wrstr(path, "02:00:00:00:00:00");
-	snprintf(path, sizeof(path), "%s/%s/functions/ecm.usb0/dev_addr", cfgfs, gname);
+	snprintf(path, sizeof(path), "%s/%s/functions/loopback.0/dev_addr", cfgfs, gname);
     wrstr(path, "02:00:00:00:00:01");
 	/* Link function into configuration */
     char link_src[256], link_dst[256];
-    snprintf(link_src, sizeof(link_src), "%s/%s/functions/ecm.usb0", cfgfs, gname);
-    snprintf(link_dst, sizeof(link_dst), "%s/%s/configs/c.1/ecm.usb0", cfgfs, gname);
+    snprintf(link_src, sizeof(link_src), "%s/%s/functions/loopback.0", cfgfs, gname);
+    snprintf(link_dst, sizeof(link_dst), "%s/%s/configs/c.1/loopback.0", cfgfs, gname);
     lkl_sys_symlink(link_src, link_dst);
 
     /* 6. Attach gadget to the dummy UDC – device-connect interrupt triggers */
@@ -137,7 +137,67 @@ static int initialize_lkl(void)
 	    printf("Failed to mount configfs\n");
 	    return -1;
 	}
+
+	dev_t dev = makedev(10, 126);
+	int mknod_result = lkl_sys_mknodat(AT_FDCWD, "/dev/raw-gadget",
+		S_IFCHR | 0700 /* S_IRUSR | S_IWUSR */, dev);
+
+	if (mknod_result != 0) {
+		printf("Create device file failed\n");
+		return -1;
+	}
 	return 0;
+}
+
+#include "rawgadgethelper.c"
+
+void load_raw_gadget() {
+	char buffer[65535];
+	int fd2 = lkl_sys_open("/proc/misc", O_RDONLY, 0);
+	lkl_sys_read(fd2, buffer, sizeof(buffer));
+	printf("%s\n", buffer);
+
+	/* 1. Open the control interface */
+	int fd = lkl_sys_open("/dev/raw-gadget", O_RDWR, 0);
+	if(fd < 0) {
+		printf("Failed to open /dev/raw-gadget\n");
+		return;
+	}
+
+	/* 2	. Tell the kernel which UDC (dummy_hcd) we attach to */
+	struct usb_raw_init init = {
+		.driver_name = "dummy_udc",
+	    .device_name  = "dummy_udc.0",
+    	.speed     = 3, // USB_SPEED_HIGH
+	};
+	int ret = lkl_sys_ioctl(fd, USB_RAW_IOCTL_INIT, (long)&init);
+	if(ret < 0) {
+		printf("ioctl INIT failed %d\n", ret);
+		return;
+	}
+	ret = lkl_sys_ioctl(fd, USB_RAW_IOCTL_RUN, 0x0);
+	if(ret < 0) {
+		printf("ioctl RUN failed %d\n", ret);
+		return;
+	}
+	struct usb_raw_event event = {};
+//		usb_raw_init(fd, USB_SPEED_HIGH, driver, device);
+//	usb_raw_run(fd);
+
+	ep0_loop(fd);
+//	while(true) {
+//		ret = lkl_sys_ioctl(fd, USB_RAW_IOCTL_EVENT_FETCH, (long)&event);
+//		if(ret < 0) {
+//			printf("ioctl FETCH failed %d\n", ret);
+//			return;
+//		}
+//		log_event(&event);
+//		if (event.type == USB_RAW_EVENT_CONNECT) {
+//			printf("COnnect!\n");
+//			process_eps_info(fd);
+//			continue;
+//		}
+//	}
 }
 
 
@@ -156,7 +216,8 @@ int LLVMFuzzerInitialize(int *argc, char ***argv)
 {
 	initialize_lkl();
 	load_module(argv[0][argc[0]-2], argv[0][argc[0]-1]);
-	load_usb_gadget();
+	//load_usb_gadget();
+	load_raw_gadget();
 	__llvm_profile_initialize_file();
 	atexit(end_fuzzing);
 
